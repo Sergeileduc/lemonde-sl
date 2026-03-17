842da985ab1332e2daef165b970820b8bca6b3b2
Sergeileduc <sergei.leduc@gmail.com>
2026-03-17 10:55:50 +0100

    test: Clean main.py for various tests.

--- main.py
+++ main.py
@@ -14,18 +14,16 @@ URL1 = os.getenv("LM_SL_TEST_URL1") or ""
 URL2 = os.getenv("LM_SL_TEST_URL2") or ""
 
 
-def runsync():
+def runsync_one():
     print("Version SYNC")
     with LeMonde() as lm:
         print(lm)
-        # lm.fetch_pdf(
-        #     url=URL1, email=email, password=password, max_img=3
-        # )  # limit 3 images for no OOM
-        # matrix = ["normal_light", "normal_dark", "mobile_light", "mobile_dark"]
-        # lm.fetch_multiple_pdf(url=URL1, email=email, password=password, matrix=matrix, max_img=3)
-        articles = lm.fetch_all_pdf(url=URL1, email=email, password=password, max_img=3)
-        for art in articles:
-            print(art)
+        art = lm.fetch_pdf(
+            url=URL1, email=email, password=password, max_img=5
+        )  # limit 5 images for no OOM
+        print(art)
+
+        # Comments
         # id = lm.extract_page_id(URL1)
         # print(f"Extracted page ID: {id}")
         # json_data = lm.fetch_comments(page_id=id, page=1, limit=5)
@@ -34,22 +32,69 @@ def runsync():
         #     print(c)
 
 
-async def runasync():
+def runsync_matrix():
+    print("Version SYNC")
+    with LeMonde() as lm:
+        print(lm)
+        matrix = ["normal_light", "normal_dark", "mobile_light", "mobile_dark"]
+        articles = lm.fetch_multiple_pdf(
+            url=URL1, email=email, password=password, matrix=matrix, max_img=5
+        )
+        for art in articles:
+            print(art)
+
+        # Comments
+        # id = lm.extract_page_id(URL1)
+        # print(f"Extracted page ID: {id}")
+        # json_data = lm.fetch_comments(page_id=id, page=1, limit=5)
+        # comments = [parse_comment(c) for c in json_data["comments"]]
+        # for c in comments:
+        #     print(c)
+
+
+def runsync_all():
+    print("Version SYNC")
+    with LeMonde() as lm:
+        print(lm)
+        articles = lm.fetch_all_pdf(url=URL1, email=email, password=password, max_img=5)
+        for art in articles:
+            print(art)
+
+        # Comments
+        # id = lm.extract_page_id(URL1)
+        # print(f"Extracted page ID: {id}")
+        # json_data = lm.fetch_comments(page_id=id, page=1, limit=5)
+        # comments = [parse_comment(c) for c in json_data["comments"]]
+        # for c in comments:
+        #     print(c)
+
+
+async def runasync_one():
     async with LeMondeAsync() as lm:
         print(lm)
-        # article = await lm.fetch_pdf(
-        #     url=URL2, email=email, password=password, mobile=True, dark=True
-        # )
-        # print(article.path, article.success, article.warning)
-        # matrix = ["normal_light", "normal_dark", "mobile_light", "mobile_dark"]
-        # articles = await lm.fetch_multiple_pdf(
-        #     url=URL2, email=email, password=password, matrix=matrix
-        # )
+        article = await lm.fetch_pdf(
+            url=URL2, email=email, password=password, mobile=True, dark=True, max_img=5
+        )
+        print(article)
 
-        # for article in articles:
+
+async def runasync_matrix():
+    async with LeMondeAsync() as lm:
+        print(lm)
+        matrix = ["normal_light", "normal_dark", "mobile_light", "mobile_dark"]
+        articles = await lm.fetch_multiple_pdf(
+            url=URL2, email=email, password=password, matrix=matrix, max_img=5
+        )
+
+        for art in articles:
+            print(art)
         #     print(article.path, article.success, article.warning)
 
-        articles = await lm.fetch_all_pdf(url=URL2, email=email, password=password, max_img=3)
+
+async def runasync_all():
+    async with LeMondeAsync() as lm:
+        print(lm)
+        articles = await lm.fetch_all_pdf(url=URL2, email=email, password=password, max_img=5)
         for art in articles:
             print(art)
 
@@ -65,6 +110,17 @@ if __name__ == "__main__":
     for art in genpdf:
         os.remove(art)
 
-    runsync()
+    # Sync
+    runsync_one()
     time.sleep(0.5)
-    asyncio.run(runasync())
+    runsync_matrix()
+    time.sleep(0.5)
+    runsync_all()
+    time.sleep(0.5)
+
+    # Async
+    asyncio.run(runasync_one())
+    time.sleep(0.5)
+    asyncio.run(runasync_matrix())
+    time.sleep(0.5)
+    asyncio.run(runasync_all())
